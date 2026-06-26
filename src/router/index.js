6226ca19c3604d900397a2f97281e016eeeb2295
KenkenPie <yapliao@gmail.com>
2026-06-26 22:19:48 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import AreaList from "../views/AreaList.vue";
 import AreaPage from "../views/AreaPage.vue";
@@ -24,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
